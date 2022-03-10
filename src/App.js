b8d1b2894fdf30271b4775569d2ccb5978d24717
offiahchiaz <offiahchiaz@gmail.com>
2022-03-10 19:27:22 +0100

    Added route guards that is dependent on a user being logged in or not

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useAppContext } from "./hooks/useAppContext";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
 //import SubNavbar from "./components/SubNavbar";
 
 
 function App() {
-  const { authIsReady } = useAppContext()
+  const { authIsReady, user } = useAppContext()
   return (
     <div className="">
       { authIsReady && (
@@ -17,13 +18,15 @@ function App() {
           <Switch>
             
               <Route exact path="/">
-            
+                <Home />
               </Route>
               <Route path="/login">
-                <Login />
+                {user && <Redirect to="/" />}
+                {!user && <Login />}
               </Route>
               <Route path="/signup">
-                <Signup />
+                {user && <Redirect to="/" />}
+                {!user && <Signup />}
               </Route>
           </Switch>
         </BrowserRouter>
